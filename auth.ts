d914520dcd0550f29b4096321152f7df47e62457
Yana <yana.kravchenko16@gmail.com>
2026-06-18 21:47:58 +0200

    added customize token with jwt callback

--- auth.ts
+++ auth.ts
@@ -49,13 +49,34 @@ export const config = {
         })
     ],
     callbacks: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         async session({ session, user, trigger, token }: any) {
             session.user.id = token.sub;
+            session.user.role = token.role;
+            session.user.name = token.name;
 
             if ( trigger ===  'update' ) {
                 session.user.name = user.name;
             }
             return session
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        async jwt({token, user, trigger, session}: any) {
+            if(user) {
+                token.role = user.role;
+
+                if (user.name === 'NO_NAME') {
+                    token.name = user.email!.split('@')[0];
+
+
+                    await prisma.user.update({
+                        where: {id: user.id},
+                        data: {name: token.name},
+                    })
+                }
+
+            }
+            return token;
         }
     }
 } satisfies NextAuthConfig ;
