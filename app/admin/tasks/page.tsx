337ef241f0c9d5f7eddaa05601e16b48a2adb818
Yana <yana.kravchenko16@gmail.com>
2026-09-18 21:48:13 +0200

    created table for admin tasks page

--- app/admin/tasks/page.tsx
+++ app/admin/tasks/page.tsx
@@ -1,4 +1,6 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getAllSubjects } from "@/lib/actions/subject.action";
+import Link from "next/link";
 
 const AdminTasksPage = async (props: {
     searchParams: Promise<{
@@ -16,7 +18,7 @@ const AdminTasksPage = async (props: {
     const task = searchParams.task || '';
     const question = searchParams.question || '';
 
-    const tasks = await getAllSubjects({
+    const subjects = await getAllSubjects({
         query: searchText,
         subject,
         task,
@@ -24,10 +26,47 @@ const AdminTasksPage = async (props: {
     });
 
     return (
-        <div className="space-y-2">
-            <div>
+        <div className="space-y-2 py-4">
+            <div className="flex-between">
                 <p className="hero-title text-4xl">Tasks</p>
+                <button className="btn-primary">
+                    <Link href='/admin/tasks/create'>
+                    Create New Task
+                    </Link>
+                </button>
             </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>ID</TableHead>
+                        <TableHead>TITLE</TableHead>
+                        <TableHead >SUBJECT</TableHead>
+                        <TableHead>DIFFICULTY</TableHead>
+                        <TableHead>AGE GROUP</TableHead>
+                        <TableHead className="w-24">ACTIONS</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {subjects.flatMap((subject) => 
+                        subject.tasks.map((task) => (
+                        <TableRow key={task.id}>
+                            <TableCell>{task.id}</TableCell>
+                            <TableCell>{task.title}</TableCell>
+                            <TableCell >{subject.name}</TableCell>
+                            <TableCell>{task.difficulty}</TableCell>
+                            <TableCell>{task.ageGroup}</TableCell>
+                            <TableCell>
+                                <button className="btn-secondary" >
+                                    <Link href={`/admin/tasks/${task.id}`}>
+                                    Edit
+                                    </Link>
+                                </button>
+                            </TableCell>
+                        </TableRow>
+                            ))
+                    )}
+                </TableBody>
+            </Table>
         </div>
     );
 }
